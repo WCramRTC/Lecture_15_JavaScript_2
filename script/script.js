ab4e73947089c2bf80abb5e5694c151c60c40171
WCram <wcram@rtc.edu>
2022-03-03 09:52:46 -0800

    Added Radio Button Example

--- script/script.js
+++ script/script.js
@@ -10,22 +10,32 @@
 const loopText = document.getElementById("text-loop");
 const loopBtn = document.getElementById("btn-loops");
 const loopDisplay = document.getElementById("loop-display");
+// let cars = document.getElementsByName("car-1");
+const btnCar = document.getElementById("btn-car");
+loopBtn.addEventListener('click', function() {
+    alert();
+});
 
 
-loopBtn.addEventListener('click', loops);
+// function loops() {
+//     loopDisplay.innerHTML = "";
+//     let count = parseInt(loopText.value);
 
-function loops() {
-    loopDisplay.innerHTML = "";
-    let count = parseInt(loopText.value);
+//     if(count > 100) count = 100;
 
-    if(count > 100) count = 100;
+//     for(let i = 0; i < count; i++) {
+//         loopDisplay.innerText += i + "\n";
+//     }
 
-    for(let i = 0; i < count; i++) {
-        loopDisplay.innerText += i + "\n";
-    }
+// } // loop
 
-} // loop
-
-function conditions() {
-
-}
+// btnCar.addEventListener('click', function() {
+//     let cars = document.getElementsByName("car-1");
+//     for(let i = 0; i < cars.length; i++) {
+//         if(cars[i].checked) {
+//             alert(cars[i].value);
+//             return;
+//         }
+//     }
+    
+// });
