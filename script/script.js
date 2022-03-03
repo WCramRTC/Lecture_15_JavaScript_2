3276dc246849b36ddf673a3d2d189b3a60b812ae
WCram <wcram@rtc.edu>
2022-03-03 11:33:18 -0800

    Updated in class examples

--- script/script.js
+++ script/script.js
@@ -12,9 +12,44 @@ const loopBtn = document.getElementById("btn-loops");
 const loopDisplay = document.getElementById("loop-display");
 // let cars = document.getElementsByName("car-1");
 const btnCar = document.getElementById("btn-car");
+
 loopBtn.addEventListener('click', function() {
-    alert();
+    // for(int i = 0; i < 10; i++) {
+
+    // }
+
+    let count = parseInt(loopText.value);
+    // if(count > 100 || count < 0) return;
+
+    loopDisplay.innerText = "";
+    
+    if(count > 100) {
+        count = 100;
+    }
+
+    else if(count < 0) {
+        count = 5;
+    }
+
+    for(let i = 0; i < count ; i++) {
+        loopDisplay.innerText += i + "\n";
+    }
+
+    // == Not type sensitive
+    // === Type sensitive
+
 });
+
+
+function thisMethod(name, num, bool) {
+    if(bool) {
+        return name;
+    }
+    else {
+        return num;
+    }
+}
+
 
 
 // function loops() {
@@ -29,13 +64,17 @@ loopBtn.addEventListener('click', function() {
 
 // } // loop
 
-// btnCar.addEventListener('click', function() {
-//     let cars = document.getElementsByName("car-1");
-//     for(let i = 0; i < cars.length; i++) {
-//         if(cars[i].checked) {
-//             alert(cars[i].value);
-//             return;
-//         }
-//     }
+btnCar.addEventListener('click', function() {
     
-// });
+    let cars = document.getElementsByName("car-1");
+    let toppings = document.getElementsByName("toppings");
+
+    for(let i = 0; i < toppings.length; i++) {
+        if(toppings[i].checked) {
+            alert(toppings[i].value);
+
+        }
+
+    }
+    
+});
